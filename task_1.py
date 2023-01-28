bde3dced18f4a1d709d86d5f3e1289ecea04ea9c
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-01-28 17:21:27 +0200

    Done

--- task_1.py
+++ task_1.py
@@ -1,8 +1,7 @@
 # Завдання 1. Додайте до класу Tree метод, який реалізує додавання до бінарного
-# дерева пошуку нових елементів зі списку. Метод має містити функціонал, який
+# дерева пошуку нових елементів. Метод має містити функціонал, який
 # перевіряє дані зі списку на відповідність до правил формування бінарного
 # дерева пошуку.
-
 
 print('\n--- Task 1 ---')
 
@@ -14,6 +13,7 @@ class BSTree:
         self.right = None
         self.val = val
 
+    # Функція вставки нового елемента.
     def insert(self, val):
         if not self.val:
             self.val = val
@@ -27,36 +27,77 @@ class BSTree:
                 self.left.insert(val)
                 return
             self.left = BSTree(val)
+            print('Added to the left:', val, '\n')
             return
 
         if self.right:
             self.right.insert(val)
             return
         self.right = BSTree(val)
+        print('Added to the right:', val)
 
+    # Функція пошуку мінімального елемента.
     def get_min(self):
         current = self
         while current.left is not None:
             current = current.left
         return current.val
 
+    # Функція пошуку максимального елемента.
     def get_max(self):
         current = self
         while current.right is not None:
             current = current.right
         return current.val
 
+    # Функція перевірки, чи існує елемент.
     def exists(self, val):
         if val == self.val:
+            print('\nFound:', val)
             return True
-        if val < self.val:
-            if self.left == None:
+
+        elif val < self.val:
+            if self.left is None:
+                print('Not found:', val, '\n')
                 return False
             return self.left.exists(val)
-        if self.right == None:
+
+        elif self.right is None:
+            print('Does not exist:', val, '\n')
             return False
         return self.right.exists(val)
 
+        # Функція видалення елемента.
+    def delete(self, val):
+        if self is None:
+            return self
+
+        elif val < self.val:
+            if self.left:
+                self.left = self.left.delete(val)
+            return self
+
+        elif val > self.val:
+            if self.right:
+                self.right = self.right.delete(val)
+            return self
+        print(val, 'was deleted')
+
+        if self.right is None:
+            return self.left
+
+        if self.left is None:
+            return self.right
+
+        min_larger_node = self.right
+        while min_larger_node.left:
+            min_larger_node = min_larger_node.left
+        self.val = min_larger_node.val
+        self.right = self.right.delete(min_larger_node.val)
+
+        return  self
+
+        # Функція друку.
     def print_tree(self):
         if self.left:
             self.left.print_tree()
@@ -65,37 +106,24 @@ class BSTree:
             self.right.print_tree()
 
 
-
-tree = BSTree(1)
-#print('tree:', tree.val)
+tree = BSTree(12)
+# print('tree:', tree.val)
 tree.left = BSTree(2)
-#print('left:', tree.left.val)
-tree.left.left = BSTree(3)
-#print('left_left:', tree.left.left.val)
-tree.left.right = BSTree(4)
-#print('left_right:', tree.left.left.val)
+# print('left:', tree.left.val)
+tree.left = BSTree(3)
+# print('left_left:', tree.left.left.val)
+tree.left = BSTree(4)
+# print('left_right:', tree.left.right.val)
 tree.right = BSTree(5)
-#print('right:', tree.right.val)
-
-#tree.insert(45)
-
-# tree.insert(12)
-# tree.insert(8)
-# tree.insert(22)
-# tree.insert(14)
-# tree.insert(4)
-# tree.insert(3)
-
-print(tree.print_tree())
+# print('right:', tree.right.val)
+tree.right.left = BSTree(23)
 
 
+tree.insert(25)
+tree.insert(7)
 
-# nodes = [7, 3, 2, 6, 9, None, 1, 5, 8, 4, 24]
-#
-# print('Given list of nodes:', nodes)
-# # Додавання елементів зі списку до дерева.
-# binary_tree = build(nodes)
-# print('Binary tree with values from the list:', binary_tree)
-#
-# # Перевіряє відповідність до правил.
-# print('Properties of the tree: \n', binary_tree.properties)
+tree.exists(7)
+tree.exists(3)
+
+print("Binary tree:")
+tree.print_tree()
--- task_2.py
+++ task_2.py
@@ -1,26 +1,10 @@
-from task_1 import BSTree, tree
+from task_1 import tree
 
 # Завдання 2. Додайте до класу Tree методи пошуку мінімального і максимального
 # значення елементів в бінарному дереві пошуку.
 
 
-print('--- Task 2 ---')
-
-
-# Знаходить мінімальне значення.
-def get_min(self):
-    current = self
-    while current.left is not None:
-        current = current.left
-    return current.val
-
-
-# Знаходить максимальне значення.
-def get_max(self):
-    current = self
-    while current.right is not None:
-        current = current.right
-    return current.val
+print('\n--- Task 2 ---')
 
 
 print('Min:', tree.get_min())
